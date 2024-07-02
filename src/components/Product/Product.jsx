0f9a8fd2659f196d9c9b8bd2b68f4c75a0d7cc88
kawsar-uddin <kawsaruddin450@gmail.com>
2024-07-02 16:04:53 +0600

    complete product section

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+    const {name, price, seller, ratings, img} = props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <h2>{name}</h2>
+            <h2 className='price-title'>Price: ${price}</h2>
+            <p>Manufacturer: {seller}</p>
+            <p>Rating: {ratings} Star</p>
+            <button className='product-cart-btn'>Add to Cart</button>
+        </div>
+    );
+};
+
+export default Product;
